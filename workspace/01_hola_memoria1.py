907ed21b929df807b78a4e735d04dc2df8ba5246
sofiabareno179-cmyk <sofiabareno179@gmail.com>
2026-03-13 16:07:53 -0500

    Create 01_hola_memoria1.py

---
+++ workspace/01_hola_memoria1.py
@@ -0,0 +1,10 @@
+#1. Creamos una caja con nuestro dinero inicial
+billetera=50000
+print(f"dinero inicial:${billetera}")
+#2. Reasinagnacion: Tomamos lo que habian en la billetera, le restamos 15000 y lo g
+billetera=billetera-15000
+print(f"compre un almuerzo.nuevo saldo:${billetera}")
+#3. Operaciones multiples
+bonificacion=20000
+billetera=billetera+bonificacion
+print (f"Recibi un pago. saldo final:${billetera}")
