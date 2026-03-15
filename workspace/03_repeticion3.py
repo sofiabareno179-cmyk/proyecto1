a11f31af5720964e4882bc23126a0b3732fd0d70
sofiabareno179-cmyk <sofiabareno179@gmail.com>
2026-03-15 14:45:10 -0500

    Create 03_repeticion3.py

---
+++ workspace/03_repeticion3.py
@@ -0,0 +1,6 @@
+ #Tabla del 5
+print("tabla de multiplicar del 5")
+#utilizar un for para recorrer y calcular en cada vuelta el resultado de la tabla del 5
+for numero in range (1,11):
+    multiplicar= 5 * numero
+    print(f" 5 X {numero} = {multiplicar}")
