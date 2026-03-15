a2e052ba26b37353f761341bbe61f9c94c02183b
sofiabareno179-cmyk <sofiabareno179@gmail.com>
2026-03-15 14:43:40 -0500

    Create perfil.py

---
+++ workspace/perfil.py
@@ -0,0 +1,10 @@
+nombre="sofia bareño"
+edad=18
+comida_favorita="pasta con albóndigas"
+#mostar un mensaje de presentacion inicial 
+print(f"hola, me llamo {nombre},tengo {edad} años y me encanta comer {comida_favorita} .")
+año_nacimiento=2007
+año_actual=2026
+edad_calculada=año_actual-año_nacimiento#calcular el año actual y el año de nacimientp para calcular la edad 
+#mostrar un mensaje con el resultado
+print(f"la edad calculada entre el año {año_actual} y el año {año_nacimiento} serian:{edad_calculada}")
