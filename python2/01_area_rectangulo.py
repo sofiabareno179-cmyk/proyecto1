e876ecb16803d4806620533877fdda6d484188c1
sofiabareno179-cmyk <sofiabareno179@gmail.com>
2026-03-15 14:46:39 -0500

    Create 01_area_rectangulo.py

---
+++ python2/01_area_rectangulo.py
@@ -0,0 +1,9 @@
+def calcular_area_rectangular(base:float,altura:float) -> float:
+    """Calcula y retorna la multiplicacion de ambos"""
+    resultado=base*altura
+    return resultado
+#resultado agregarla en una variable
+resultado_area=calcular_area_rectangular(6.0,4.5)
+print(f"resultado del area es:{resultado_area}")
+
+    
